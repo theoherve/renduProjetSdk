633a8a707f63ee3a9ce27c867962eb495ae59dfb
Alexandre Hardy <hardyalexandre2000@gmail.com>
2022-07-03 22:03:37 +0200

    hotfix discord

--- client/class/DiscordProvider.class.php
+++ client/class/DiscordProvider.class.php
@@ -3,7 +3,7 @@
 class DiscordProvider{
 
     public static function Discord(){
-        return new Provider(DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, "http://localhost:8081/discord_callback", "identify email", "POST", "https://discord.com/api/oauth2/token", "https://discord.com/api/oauth2/@me");
+        return new Provider(DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, "http://localhost:8081/discord_callback", "identify email", "POST", "https://discord.com/api/oauth2/token", "https://discord.com/api/users/@me");
     }
 
 }
